99a758620dbad6f956d88548b63bf7e40a28f71d
João Vitor Liberato <j.liberato@vertex.vertexdigital.co>
2025-12-04 03:17:23 -0300

    git add .

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
   <div
     ref="remote"
   />
@@ -14,38 +14,4 @@
     mod.default.mount(remote.value!)
   })
 
-</script> -->
-
-<!-- src/App.vue do shell (container principal) -->
-<template>
-  <div>
-    <h1>Shell rodando perfeitamente!</h1>
-    <button @click="loadHome">Carregar MF-Home</button>
-    <div id="mf-home-container"></div>
-  </div>
-</template>
-
-<script setup lang="ts">
-async function loadHome() {
-  try {
-    // Carrega o remote mf-home
-    const { mount } = await import('mf_home/App')
-    
-    // Monta no container
-    mount(document.getElementById('mf-home-container')!)
-    
-    console.log('mf-home carregado com sucesso!')
-  } catch (err) {
-    console.error('Erro ao carregar mf-home:', err)
-  }
-}
 </script>
-
-<style>
-#mf-home-container {
-  margin-top: 20px;
-  padding: 20px;
-  border: 2px solid #42b983;
-  border-radius: 8px;
-}
-</style>
